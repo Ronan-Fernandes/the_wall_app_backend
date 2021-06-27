27806534329eceb89b57baf111cfd2651b47ebd3
Ronan-Fernandes <ronanf.sk@gmail.com>
2021-06-26 22:36:42 -0300

    new test cases for login

--- __tests__/routes/userRoutes.test.js
+++ __tests__/routes/userRoutes.test.js
@@ -5,11 +5,12 @@ const supertest = require('supertest');
 const TEST_DATABASE = 'the_wall_app_tests';
 const COLLECTION = 'users';
 const user = {
-  name: "Ronan Fernandes",
+  name: "user name",
   email: "email@email.com",
   password: "123456789"
-}
+};
 
+const userHash = "$2a$10$q3C9I2uWtkdyNXaIdm.1nO9hP5EkmREF0nApQWO6irY6XboOg1rTy";
 
 describe("User route tests", () => {
 
@@ -29,24 +30,24 @@ describe("User route tests", () => {
   });
 
   test("POST /user  Create user.", async () => {
-    
-    const response = await supertest(app).post('/user').send(user);
+
+    const response = await supertest(app).post('/user').send({ ...user, confirm_password: '123456789' });
 
     expect(response.statusCode).toEqual(201);
     expect(response.body.message).toEqual("User registred with success!");
   });
 
   test("POST /user  Create user when user already exists.", async () => {
-    await client.db(TEST_DATABASE).collection(COLLECTION).insertOne({...user});
+    await client.db(TEST_DATABASE).collection(COLLECTION).insertOne({ ...user, password: userHash });
 
-    const response = await supertest(app).post('/user').send({ ...user,  confirm_password: '123456789'});
+    const response = await supertest(app).post('/user').send({ ...user, confirm_password: '123456789' });
 
     expect(response.statusCode).toEqual(409);
     expect(response.body.error).toEqual("User already exists!");
   });
 
   test("POST /user/login", async () => {
-    const createdUser = await client.db(TEST_DATABASE).collection(COLLECTION).insertOne({...user});
+    const createdUser = await client.db(TEST_DATABASE).collection(COLLECTION).insertOne({ ...user, password: userHash });
 
     const response = await supertest(app).post('/user/login').send({
       email: user.email,
@@ -57,6 +58,44 @@ describe("User route tests", () => {
     expect(response.body.token).toBeDefined();
     expect(response.body.userId).toEqual(createdUser.ops[0]._id);
     expect(response.body.name).toEqual(createdUser.ops[0].name);
+  });
+
+  test("POST /user/login when user does not exist.", async () => {
+
+    const response = await supertest(app).post('/user/login').send({
+      email: user.email,
+      password: user.password
+    });
+
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.token).not.toBeDefined();
+    expect(response.body.message).toEqual('Unauthorized, wrong email or password');
+  });
+
+  test("POST /user/login when password is wrong", async () => {
+    await client.db(TEST_DATABASE).collection(COLLECTION).insertOne({ ...user, password: userHash });
+
+    const response = await supertest(app).post('/user/login').send({
+      email: user.email,
+      password: user.password + '1'
+    });
+
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.token).not.toBeDefined();
+    expect(response.body.message).toEqual('Unauthorized, wrong email or password');
+  });
+
+  test("POST /user/login when email is wrong", async () => {
+    await client.db(TEST_DATABASE).collection(COLLECTION).insertOne({ ...user, password: userHash });
+
+    const response = await supertest(app).post('/user/login').send({
+      email: 'a' + user.email,
+      password: user.password
+    });
+
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.token).not.toBeDefined();
+    expect(response.body.message).toEqual('Unauthorized, wrong email or password');
   });
 
 });
