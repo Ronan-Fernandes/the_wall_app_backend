67396258398afffdf6a1ed4e041f783449010145
Ronan-Fernandes <ronanf.sk@gmail.com>
2021-06-30 00:08:59 -0300

    POST /post create post tets

--- __tests__/routes/postsRoutes.test.js
+++ __tests__/routes/postsRoutes.test.js
@@ -17,6 +17,11 @@ const postsWithObjectId = posts.map((post) => ({
   _id: ObjectId(post._id),
 }));
 
+const usersWithObejectId = users.map((user) => ({
+  ...user,
+  _id: ObjectId(user._id),
+}));
+
 describe("Posts routes tests", () => {
   let client;
 
@@ -32,7 +37,7 @@ describe("Posts routes tests", () => {
     await client.close();
   });
 
-  test("GET /posts should get all the posts", async () => {
+  test("GET /post should get all the posts", async () => {
     await client.db(TEST_DATABASE).collection(POSTS_COLLECTION).insertMany(postsWithObjectId);
 
     const response = await supertest(app).get("/post");
@@ -40,5 +45,40 @@ describe("Posts routes tests", () => {
     expect(response.statusCode).toEqual(200);
     expect(response.body.length).toBeGreaterThan(0);
     expect(response.body).toStrictEqual(posts);
+  });
+
+  test("POST /post should create a new post", async () => {
+    await client.db(TEST_DATABASE).collection(USERS_COLLECTION).insertOne({
+      ...usersWithObejectId[0],
+      password: usersWithObejectId[0].hash,
+    });
+
+    const authResponse = await supertest(app).post("/user/login").send({
+      email: users[0].email,
+      password: users[0].password,
+    });
+
+    const response = await supertest(app).post("/post").send({
+      title: posts.title,
+      content: posts.content,
+    })
+      .set("Accept", "application/json")
+      .set("Authentication", authResponse.body.token);
+
+    expect(response.statusCode).toEqual(201);
+    expect(response.body.message).toEqual("Post succefully created");
+  });
+
+  test("POST /post can't create post without auth", async () => {
+    await client.db(TEST_DATABASE).collection(USERS_COLLECTION).insertMany(usersWithObejectId);
+
+    const response = await supertest(app).post("/post").send({
+      title: posts.title,
+      content: posts.content,
+    })
+      .set("Accept", "application/json");
+
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.message).toEqual("Unauthorized");
   });
 });
